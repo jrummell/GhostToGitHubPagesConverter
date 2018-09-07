bde454028f9db7d2308f39a24d6f08d496655da2
John Rummell <john@elitebusinesssolutions.com>
2018-09-07 12:59:28 -0400

    Added pages support

--- GhostToGitHubPagesConverter/Program.cs
+++ GhostToGitHubPagesConverter/Program.cs
@@ -20,39 +20,45 @@ namespace GhostToGitHubPagesConverter
 
                 foreach (Db db in export.db)
                 {
-                    var postsPath = Path.Combine(db.meta.exported_on.ToString(), "_posts");
-                    var directory = new DirectoryInfo(postsPath);
-                    if (!directory.Exists)
+                    var dbDirectory = new DirectoryInfo(db.meta.exported_on.ToString());
+                    if (!dbDirectory.Exists)
                     {
-                        directory.Create();
+                        dbDirectory.Create();
                     }
 
-                    foreach (Post post in db.data.posts)
+                    var postsPath = Path.Combine(dbDirectory.FullName, "_posts");
+                    var postDirectory = new DirectoryInfo(postsPath);
+                    if (!postDirectory.Exists)
                     {
-                        var tagIds = db.data.posts_tags.Where(x => x.post_id == post.id)
-                                       .Select(x => x.tag_id)
-                                       .ToArray();
+                        postDirectory.Create();
+                    }
 
-                        var tags = db.data.tags.Where(t => tagIds.Contains(t.id))
-                                     .Select(t => t.name)
-                                     .ToArray();
+                    var posts = db.data.posts.Where(p => p.page == 0)
+                                  .ToArray();
 
-                        var published = post.status == "published" && post.visibility == "public";
+                    foreach (Post post in posts)
+                    {
+                        string[] tags = GetTags(db, post);
+                        string frontMatter = GetFrontMatter(post, tags);
+                        string postContent = GetContent(post, frontMatter);
 
-                        // see https://help.github.com/articles/configuring-jekyll/#front-matter-is-required and https://jekyllrb.com/docs/front-matter/
-                        string frontMatter = $@"---
-layout: post
-title: {post.title?.Replace(":", " -")}
-permalink: /{post.slug}
-date: {post.published_at ?? post.created_at}
-published: {published.ToString().ToLower()}
-tags: {string.Join(" ", tags)}
----";
+                        string fileName = (post.published_at ?? post.created_at).Split(" ")
+                                                                                .First() + "-" + post.slug + ".md";
+                        string fullPath = Path.Combine(postDirectory.FullName, fileName);
+                        File.WriteAllText(fullPath, postContent);
+                    }
 
-                        string postContent = frontMatter + Environment.NewLine + Environment.NewLine + post.markdown;
+                    var pages = db.data.posts.Where(e => e.page == 1)
+                                  .ToArray();
 
-                        string fileName = (post.published_at ?? post.created_at).Split(" ").First() + "-" + post.slug + ".md";
-                        string fullPath = Path.Combine(directory.FullName, fileName);
+                    foreach (Post post in pages)
+                    {
+                        string[] tags = GetTags(db, post);
+                        string frontMatter = GetFrontMatter(post, tags);
+                        string postContent = GetContent(post, frontMatter);
+
+                        string fileName = post.slug + ".md";
+                        string fullPath = Path.Combine(dbDirectory.FullName, fileName);
                         File.WriteAllText(fullPath, postContent);
                     }
                 }
@@ -62,6 +68,40 @@ tags: {string.Join(" ", tags)}
                 Console.WriteLine("Missing file argument");
                 Environment.Exit(1);
             }
+        }
+
+        private static string GetContent(Post post, string frontMatter)
+        {
+            return frontMatter + Environment.NewLine + Environment.NewLine + post.markdown;
+        }
+
+        private static string GetFrontMatter(Post post, string[] tags)
+        {
+            var layout = post.page == 0 ? "post" : "default";
+            var published = post.status == "published" && post.visibility == "public";
+
+            // see https://help.github.com/articles/configuring-jekyll/#front-matter-is-required and https://jekyllrb.com/docs/front-matter/
+            string frontMatter = $@"---
+layout: {layout}
+title: {post.title?.Replace(":", " -")}
+permalink: /{post.slug}
+date: {post.published_at ?? post.created_at}
+published: {published.ToString().ToLower()}
+tags: {string.Join(" ", tags)}
+---";
+            return frontMatter;
+        }
+
+        private static string[] GetTags(Db db, Post post)
+        {
+            var tagIds = db.data.posts_tags.Where(x => x.post_id == post.id)
+                           .Select(x => x.tag_id)
+                           .ToArray();
+
+            var tags = db.data.tags.Where(t => tagIds.Contains(t.id))
+                         .Select(t => t.name)
+                         .ToArray();
+            return tags;
         }
     }
 }
